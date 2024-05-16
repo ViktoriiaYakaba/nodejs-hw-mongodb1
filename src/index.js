010fd847d2658b2b2ec8e19e36c1c05927200e60
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-16 16:51:12 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { initMongoConnection } from './db/initMongoConnection.js';
 
 const bootstrap = async () => {
     await initMongoConnection();
-    setupServer
+    setupServer();
 }
 
 bootstrap();
