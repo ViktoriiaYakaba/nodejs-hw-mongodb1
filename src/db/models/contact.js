e802eb5b8cce619cb604997e7d8076b7723f2126
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-16 17:18:08 +0200

    update server.js

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -38,4 +38,4 @@ const contactsSchema = new Schema(
 }
 );
 
-export const ContactsCollection = model('students', contactsSchema);
+export const ContactsCollection = model('contacts', contactsSchema);
